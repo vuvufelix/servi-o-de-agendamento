e85fdff16033c56190a24e8463b1ac6ad437588f
vuvufelix <vuvufelix7@gmail.com>
2025-10-15 23:33:59 -0300

    Adicionado aquivo README.md no site

---
+++ README.md
@@ -0,0 +1,17 @@
+# Website for Booking Services
+
+This is an application, specifically a modern and responsive fictional landing page that displays information about booking services in Angola and abroad.
+
+![imagem da aplicação](./src/img/screencapture-127-0-0-1-5500-index-html-2025-09-24-18_16_03.png)
+
+# Bibiotecas 
+
+- Scroll Reveal: for animations of elements
+- Swipper: scroll on cards image
+
+# How Use
+
+```bach/shell
+    clone: https://github.com/vuvufelix/servi-o-de-agendamento.git
+    OPEN http://localhost:3000
+```
--- src/js/animaScroll.js
+++ src/js/animaScroll.js
@@ -2,42 +2,49 @@ export function servico_scrool() {
     ScrollReveal().reveal('.servico-scroll', { 
         origin: "top",
         duration: 1000,
-        distance: "70%" 
+        distance: "70%",
+        reset: false
     });
 
     ScrollReveal().reveal('.paragrafo-scroll', { 
         origin: "left",
         duration: 900,
-        distance: "80%" 
+        distance: "80%",
+        reset: false
     });
 
     ScrollReveal().reveal('h1', { 
         origin: "left",
         duration: 900,
-        distance: "70%" 
+        distance: "70%", 
+        reset: false
     });
 
     ScrollReveal().reveal('#artigo-conteudo', { 
         origin: "right",
         duration: 4000,
-        distance: "50%" 
+        distance: "50%",
+        reset: false
     });
 
     ScrollReveal().reveal('.scroll-localizacao', { 
         origin: "top",
         duration: 800,
-        distance: "100%" 
+        distance: "100%",
+        reset: false
     });
 
     ScrollReveal().reveal('#form-container', { 
         origin: "right",
         duration: 1000,
-        distance: "50%" 
+        distance: "50%",
+        reset: false
     });
 
     ScrollReveal().reveal('.swiper-slide', { 
         origin: "left",
         duration: 1000,
-        distance: "100%" 
+        distance: "100%",
+        reset: false
     });
 }
